9a73b3ee1f5abbad2ab2b6978d1ffde45621ef71
RickFord117 <rhenriques@spartaglobal.com>
2018-07-31 16:24:39 +0100

    proxy template tests passed

--- Berksfile
+++ Berksfile
@@ -1,6 +1,4 @@
 # frozen_string_literal: true
 source 'https://supermarket.chef.io'
 
-cookbook 'nginx', '~> 8.1.5'
-
 metadata
--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -8,3 +8,7 @@
 describe package ('nginx') do
   it { should be_installed }
 end
+
+describe http("http://localhost", enable_remote_worker: true) do
+  its('status') { should cmp 502 }
+end
