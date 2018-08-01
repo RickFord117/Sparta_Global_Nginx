b09e90afe547652e15f5f15548f8a9c7e24afbf2
RickFord117 <rhenriques@spartaglobal.com>
2018-08-01 14:40:47 +0100

    updated recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,7 +9,7 @@ end
 
 package 'nginx'
 
-service 'nginx-cookbook' do
+service 'nginx' do
   supports status: true, restart: true, reload: true
   action [:enable, :start]
 end
@@ -17,15 +17,15 @@ end
 template '/etc/nginx/sites-available/proxy.conf' do
   source 'proxy.conf.erb'
 
-  notifies :restart, 'service[nginx-cookbook]'
+  notifies :restart, 'service[nginx]'
 end
 
 link '/etc/nginx/sites-enabled/proxy.conf' do
   to '/etc/nginx/sites-available/proxy.conf'
-  notifies :restart, 'service[nginx-cookbook]'
+  notifies :restart, 'service[nginx]'
 end
 
 link '/etc/nginx/sites-enabled/default' do
   action :delete
-  notifies :restart, 'service[nginx-cookbook]'
+  notifies :restart, 'service[nginx]'
 end
